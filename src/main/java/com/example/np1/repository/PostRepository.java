2f9ca9284b2ed94b5bd579d2563a9a12d6d9a417
devsince2021 <dev.ryu2021@gmail.com>
2024-02-21 02:35:28 +0900

    @Query 어노테이션으로 N + 1 해결

--- src/main/java/com/example/np1/repository/PostRepository.java
+++ src/main/java/com/example/np1/repository/PostRepository.java
@@ -2,6 +2,11 @@ package com.example.np1.repository;
 
 import com.example.np1.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+  @Query(value = "SELECT p FROM Post p JOIN FETCH p.comments")
+  List<Post> findAll();
 }
